372df9722d2052a9c7db9a52396c58de568602d2
yougovmauro2926 <mauro.huber@yougov.ch>
2026-01-29 20:59:56 +0100

    corrected

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ module "blog_autoscaling" {
   instance_type        = var.instance_type
   image_id             = data.aws_ami.app_ami.id
   
-  traffic_source_attachments " {
+  traffic_source_attachments = {
     blog-alb = {
        traffic_source_identifier = aws_lb_target_group.blog.arn
     }
