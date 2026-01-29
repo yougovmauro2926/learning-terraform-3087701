5e8a6cbf1186b246c1367c432fa1fae099c4ab0e
yougovmauro2926 <mauro.huber@yougov.ch>
2026-01-29 20:58:45 +0100

    new asg

--- main.tf
+++ main.tf
@@ -103,3 +103,26 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_id        = aws_instance.blog.id
   port             = 80
 }
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.1.0"
+
+
+  name = "blog"
+
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+
+  launch_tamplate_name = "blog"
+  security_groups      = [module.blog_sg.security_group_id]
+  instance_type        = var.instance_type
+  image_id             = data.aws_ami.app_ami.id
+  
+  traffic_source_attachments " {
+    blog-alb = {
+       traffic_source_identifier = aws_lb_target_group.blog.arn
+    }
+  }
