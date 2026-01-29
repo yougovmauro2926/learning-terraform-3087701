ff6c779a87025d34779f4cb4f61bc9386fdd865a
yougovmauro2926 <mauro.huber@yougov.ch>
2026-01-29 20:41:54 +0100

    Closed module

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ module "blog_alb" {
   subnets = "module.blog_vpc.public_subnets"
 
   security_groups = [module.blog_sg.security_group_id]
-
+}
   listeners = {
     blog-http = {
       port     = 80
