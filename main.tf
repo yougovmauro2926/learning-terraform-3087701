71b333a6a2528ca2065d70da95995cf85d78ef7c
yougovmauro2926 <mauro.huber@yougov.ch>
2026-01-29 20:19:08 +0100

    New VPC

--- main.tf
+++ main.tf
@@ -14,8 +14,23 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true 
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -29,12 +44,13 @@ resource "aws_instance" "blog" {
   }
 }
 
+subnet_id = module.blog_vpc.public_subnets[0]
 
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
-  vpc_id =data.aws_vpc.default.id
+  vpc_id =module.blog_vpc.vpc_id
 }
 
 module "blog_sg" {
