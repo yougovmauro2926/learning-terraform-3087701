f5d9042660ee3ae17a9df8d4d03a548f79bedbe5
yougovmauro2926 <mauro.huber@yougov.ch>
2026-01-29 20:45:52 +0100

    remove quotes

--- main.tf
+++ main.tf
@@ -71,8 +71,8 @@ module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
-  vpc_id  = "module.blog_vpc.vpc_id"
-  subnets = "module.blog_vpc.public_subnets"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
 
   security_groups = [module.blog_sg.security_group_id]
 
