eb81a166909a53b38c32e3ec4649d678e7424ed5
yougovmauro2926 <mauro.huber@yougov.ch>
2026-01-29 14:36:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "Blog" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
