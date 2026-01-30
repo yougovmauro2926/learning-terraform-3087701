3d04ca72cf8a3b2ffe73b3ac53916ca8bb5f55c8
yougovmauro2926 <mauro.huber@yougov.ch>
2026-01-30 09:10:14 +0100

    refactor to variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["var.ami_filter.name"]
   }
 
   filter {
@@ -11,25 +11,24 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
-}
+  owners = [var.ami_filter.owner] }
 
   
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["${var.environment.network_prefix}.1.0/24", "${var.environment.network_prefix}.2.0/24", "${var.environment.network_prefix}.3.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -74,7 +73,7 @@ module "blog_alb" {
   }
 
   tags = {
-    Environment = "Dev"
+    Environment = var.environment.name
   }
 }
 
@@ -91,8 +90,8 @@ module "blog_autoscaling" {
 
   name = "blog"
 
-  min_size = 1
-  max_size = 2
+  min_size = var.min_size
+  max_size = var.max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
 
--- variables.tf
+++ variables.tf
@@ -1,4 +1,38 @@
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
+
+}
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+  type        = object ({
+    name      = string
+    owner     = string
+  })
+
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
+}
+variable "environment" {
+  description = "Deployment Environment"
+  type        = object ({
+    name           = string
+    network_prefix = string
+  })
+  default     = {
+    name           = "dev"
+    network_prefix = "10.0"
+  }
+}  
+
+variable "min_size" {
+  description = "Minimum number of instances in ASG"
+  default = 1
+}
+variable "max_size" {
+  description = "Maximal number of instances in ASG"
+  default = 2
 }
