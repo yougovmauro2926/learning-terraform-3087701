8e8dd7afa524358eba2ea871444fcb9c0f9b2572
yougovmauro2926 <mauro.huber@yougov.ch>
2026-01-30 08:26:47 +0100

    removed LB from where it wasn't needed

--- main.tf
+++ main.tf
@@ -85,12 +85,6 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
-}
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.1.0"
